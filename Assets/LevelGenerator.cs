afdc1c25ccb21c37a5935dee093fb96303b4b705
Abby <4015849@g.highlineschools.org>
2024-03-05 13:36:51 -0800

    2 paddle ball and beginning of brick

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Vector2Int size;
+    public Vector2 offset;
+    public GameObject brickPrefab;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/paddleControl.cs
+++ Assets/paddleControl.cs
@@ -1,27 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Timeline;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class paddleControl : MonoBehaviour
 {
-    Vector2 movementInput;
-    Rigidbody2D rb;
-
+    public float speed=5;
+    public float maxX=7.5f;
+    float movementHorizontal;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void OnMove(InputValue movementValue)
-    {
-        movementInput = movementValue.Get<Vector2>();
+        movementHorizontal = Input.GetAxis("Horizontal");
+        if((movementHorizontal>0 && transform.position.x < maxX) || (movementHorizontal<0 && transform.position.x > -maxX))
+        {
+            transform.position += Vector3.right * movementHorizontal * speed * Time.deltaTime;
+        }
     }
 }
