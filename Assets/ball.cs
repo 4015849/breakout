28a8db70b6dbcf0bc4666cb345ed0f5a65ed75fe
Abby <4015849@g.highlineschools.org>
2024-03-06 13:40:41 -0800

    3 brick/level generation, adjustments to sizing for multiple elements, ui elements added, tmpro files

--- Assets/ball.cs
+++ Assets/ball.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ball : MonoBehaviour
 {
     public float minY = -5.5f;
     public float maxVelocity = 15f;
     Rigidbody2D rb;
+    int score = 0;
+    int lives = 5;
+    public TextMeshProUGUI scoreTxt;
+    public GameObject[] livesImage;
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +24,18 @@ public class ball : MonoBehaviour
     {
         if(transform.position.y < minY)
         {
-            transform.position = Vector3.zero;
-            rb.velocity = Vector3.zero;
+            if (lives <= 0)
+            {
+                GameOver();
+            }
+            else
+            {
+                transform.position = Vector3.down;
+                rb.velocity = Vector3.zero;
+                lives--;
+                livesImage[lives].SetActive(false);
+            }
+
         }
 
         if(rb.velocity.magnitude > maxVelocity)
@@ -34,6 +49,13 @@ public class ball : MonoBehaviour
         if (collision.gameObject.CompareTag("Brick"))
         {
             Destroy(collision.gameObject);
+            score += 10;
+            scoreTxt.text = score.ToString("00000");
         }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Game Over");
     }
 }
